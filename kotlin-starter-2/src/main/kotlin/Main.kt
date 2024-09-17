b46d98aa1ed886db5730cabebd642789f00eebf4
Nikolas Henrik Owen <niko.owen88@gmail.com>
2024-09-18 01:49:41 +0700

    Exception Handling

--- kotlin-starter-2/src/main/kotlin/Main.kt
+++ kotlin-starter-2/src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package com.dicoding.kotlin
 
+import kotlin.NumberFormatException
+
 //cara lain -> import packagenya agar efisien
 //import com.dicoding.oop.utils.sayHello
 //import com.dicoding.oop.utils.PI
@@ -19,4 +21,55 @@ fun main(){
     println(pow(3.0, 2.0))
     println(PI)
     println(areaOfCircle(13.0))
+
+    /*
+    --------- EXCEPTION ---------
+    ArithmeticException -> bilangan dibagi nol
+    NumberFormatException -> format valuenya tidak sesuai
+    NullPointerException -> valuenya null
+     */
+
+    /*
+    -------- EXCEPTION HANDLING -------
+    try {
+    // Block try, menyimpan kode yang membangkitkan exception
+    } catch (e: SomeException) {
+        // Block catch akan terpanggil ketika exception bangkit.
+    } finally {
+    }
+     */
+    val someNullValue: String? = null
+    lateinit var someMustNotNullValue: String
+
+    try {
+        someMustNotNullValue = someNullValue!!
+        println(someMustNotNullValue)
+    } catch (e: Exception) {
+        someMustNotNullValue = "Nilai String Null"
+        //println(someMustNotNullValue) -> dipindah ke finally
+    } finally {
+        println(someMustNotNullValue)
+    }
+
+    /*
+    ------------------------------------
+        Bisa juga multiple catch
+     */
+
+    val someStringValue: String? = null
+    var someIntValue: Int = 0
+
+    try {
+        someIntValue = someStringValue!!.toInt()
+    } catch (e: NullPointerException) {
+        someIntValue = 0
+    } catch (e: NumberFormatException) {
+        someIntValue = -1
+    } finally {
+        when(someIntValue){
+            0 -> println("Catch block NullPointerException terpanggil !")
+            -1 -> println("Catch block NumberFormatException terpanggil !")
+            else -> println(someIntValue)
+        }
+    }
 }
